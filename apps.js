15cdc3c7ed68209827f9a822725964650d88ad74
Alexa Strobel <alexa.strobel@gmail.com>
2017-12-28 16:52:28 +0800

    third commit

--- apps.js
+++ apps.js
@@ -17,6 +17,9 @@ var accessKey = Object.keys(possibleAnswers2);
 const correctAnswers = [2,3,2,1,0,3,0,3,0,1];
 const correctAnswersText = ['30','16-18','100','40','Bezoar','Cyprus','Singapura','20,000','Felicette in 1963','Shaved eyebrows']
 const catPhotos = ['cat-teeth.jpg','sleeping-cat.jpg','cat-meow.jpg','cat-breeds.jpg','hairball-cat.jpg','old-cat.jpg','tiny-cat.jpg','cat-hair.jpg','space-cat.jpg','crazy-cat.jpg']
+const positivePhrases = ['You are a cat conoisseur!','Congratulations!','Well done!','Good job!','Purr purr','You got it!','Amazing work!','Wowee!','Super duper!','Excellent work','Meow!']
+const negativePhrases = ['Grrr!','You must be a dog lover!','Boooo!','Awww man!','Not exactly!','Almost there!','Not quite!','Keep trying!','Woof woof!','Bad luck','Oops!']
+const buttonPhrases = ['Next one','Let me try again','Give me a harder question','Give me another question','More cat questions!']
 
 // counter to keep track of question numbers
 let questionNumber = 0;
@@ -35,6 +38,19 @@ const checkAnswer = function(checkedButton,questionNumber,correctAnswers) {
 	// }
 }
 
+// create a function to generate a random comment on solutions page
+const randomPositive = function(positivePhrases) {
+	return positivePhrases[Math.floor(Math.random() * positivePhrases.length)] // random number
+}
+
+const randomNegative = function(negativePhrases) {
+	return negativePhrases[Math.floor(Math.random() * negativePhrases.length)] // random number
+}
+
+const randomButtonPhrase = function(buttonPhrases) {
+	return buttonPhrases[Math.floor(Math.random() * buttonPhrases.length)] // random number
+}
+
 //starting jQuery
 $(function() {
 	// creating a function to go from initial page to quiz questions
@@ -48,20 +64,25 @@ $(function() {
 	$(document).on('click','#answer-submission',function(event) {
 		event.preventDefault()
 		let checkedButton = $('input[name=answer]:checked').val();
+		removeCheckRadioButton()
 		let checkAnswerResult = checkAnswer(checkedButton,questionNumber,correctAnswers)
 
 		$(this).parents('div.cat-questions').toggleClass('hidden')
 		let placeholder = $(this).parents('.cat-questions').siblings('.solution')
 		placeholder.toggleClass('hidden')
 			if (checkAnswerResult == 1) {
-				placeholder.children('p:first-of-type').text('Meow! You are a cat conoisseur!')
-				placeholder.children('button').text('Too easy! Give me a harder question!')
+				placeholder.children('p:first-of-type').text(randomPositive(positivePhrases))
+				placeholder.children('p').attr('class','correct-answer')
+				placeholder.children('button').text(randomButtonPhrase(buttonPhrases))
 				numberCorrect = numberCorrect + 1
+				placeholder.find('img').attr('src','photos/' + 'ragdoll-cats.jpg')
 			}
 			else {
-				placeholder.children('p').text('Grrr! You must be a dog lover!')
-				placeholder.children('button').text('Woof woof! Give me another question!')
+				placeholder.children('p:first-of-type').text(randomNegative(negativePhrases))
+				placeholder.children('p').attr('class','incorrect-answer')
+				placeholder.children('button').text(randomButtonPhrase(buttonPhrases))
 				numberWrong = numberWrong + 1
+				placeholder.find('img').attr('src','photos/' + 'pointy-ears-cats.jpg')
 			}
 		placeholder.children('p:last-of-type').text('The correct answer is: ' + correctAnswersText[questionNumber])
 
@@ -70,7 +91,12 @@ $(function() {
 
 		questionNumber = questionNumber + 1
 	})
-
+		
+const removeCheckRadioButton = function() {
+	$('input[type=radio]').prop('checked','false')
+	// $('input[type=radio]~label').css('color','white')
+	console.log('hello')	
+}
 
 // create a function to get next question
 	$(document).on('click','#next-question',function(event) {
@@ -80,36 +106,27 @@ $(function() {
 		if (questionNumber == 10) {	
 			let placeholder = $(this).parent().siblings('.end-page')
 			placeholder.toggleClass('hidden')
-			placeholder.find('p').html('You finished the quiz! Your score is ' + numberCorrect + ' out of 10' )
+			placeholder.find('h1').html('You finished the quiz! Your score is ' + numberCorrect + ' out of 10' )
 		}
 
 		else {
 			let placeholder = $(this).parent().siblings('.cat-questions')
 			placeholder.toggleClass('hidden')
 			placeholder.find('p').html(questions[questionNumber])
-			// console.log(possibleAnswers[questionNumber[0]])
-			$('div .question:nth-child(2)').children('label').html(possibleAnswers[questionNumber][0])
-			$('div .question:nth-child(3)').children('label').html(possibleAnswers[questionNumber][1])
-
-			// placeholder.find('label:first-of-type').html(possibleAnswers2.[0])
-
-			placeholder.find('label:nth-of-type(2)').html(possibleAnswers[questionNumber][1])
-			placeholder.find('label:nth-of-type(3)').html(possibleAnswers[questionNumber][2])
-			placeholder.find('label:last-of-type').html(possibleAnswers[questionNumber][3])
+			placeholder.find('.question-option:first-of-type').children('label').html('<span></span>' + possibleAnswers[questionNumber][0])
+			placeholder.find('.question-option:nth-of-type(2)').children('label').html('<span></span>' + possibleAnswers[questionNumber][1])
+			placeholder.find('.question-option:nth-of-type(3)').children('label').html('<span></span>' + possibleAnswers[questionNumber][2])
+			placeholder.find('.question-option:last-of-type').children('label').html('<span></span>' + possibleAnswers[questionNumber][3])
+			placeholder.find('img').attr('src','photos/' + catPhotos[questionNumber])
 		}
-
-		$('.cat-photo img').attr('src','photos/' + catPhotos[questionNumber])
 	})
-
-
-// create a function to change the image displayed
 
 // create a function to enter final page after last question, option to restart
 	$(document).on('click','#restart',function(event) {
 		event.preventDefault()
-		location.reload()
+		window.location.reload(true)
 
-// function to reset the variables
+// function to reset the variables or they already reset during refresh
 
 	})
 
